7afd406d8f77bbaf582e466101b8e100e193c441
Waalfegor <100232625+Waalfegor@users.noreply.github.com>
2023-08-17 17:43:27 +0300

    Adding scrollers

---
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home( String name, Model model) {
+        model.addAttribute("title","Главная страничка");
+        return "home";
+    }
+
+    @GetMapping("/contact")
+    public String contact( String name, Model model) {
+        model.addAttribute("contact","Contact");
+        return "contact";
+    }
+}
+
+
